b8a8952a2f12d9f47b44bfbc75d92ebf4c0e4078
Dulana Avishka <dulana.avishka@outlook.com>
2026-09-20 19:43:22 +0530

    add alt for image

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function ProductCard({product}) {
   return (
     <div className="product-card">
-      <img src={product.image} className="product-card-image" />
+      <img src={product.image} className="product-card-image" alt={product.description}/>
       <div className="product-card-content">
         <h3>{product.name}</h3>
         <p>{product.price}$</p>
